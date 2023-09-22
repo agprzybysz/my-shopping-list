8c240ff45ebb676e6fbca09d5f843c7ed368ad40
Agnieszka <agnieszka.przybysz31@gmail.com>
2023-09-22 23:43:42 +0200

    edit notfication messages, which display snackbar

--- client/src/configs/notificationMessages.ts
+++ client/src/configs/notificationMessages.ts
@@ -3,5 +3,8 @@ export const NOTIFICATION_MESSAGES = {
   SUCCESS: {
     LIST_CREATED: "New shopping list was created",
     LIST_DELETED: "List was removed",
+    PRODUCT_DELETED: "Product was removed from shopping list",
+    PRODUCT_ADDED: "New product added to shopping list",
+    PRODUCT_EDITTED: "Product was editted",
   },
 };
